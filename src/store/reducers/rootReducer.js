31d2a566f20d9a4b26c440fa38835564e62ce8bb
Justin Shaw <jawshaw95@gmail.com>
2020-06-28 19:11:01 -0500

    cleanup

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,16 +1,12 @@
-import authReducer from './authReducer';
-import projectReducer from './projectReducer';
-import {
-    firebaseReducer
-  } from 'react-redux-firebase'
-  import {firestoreReducer } from 'redux-firestore'
-import {combineReducers} from 'redux'
+// import authReducer from './authReducer';
+// import projectReducer from './projectReducer';
+import { firebaseReducer } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
+import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-    auth: authReducer,
-    project: projectReducer,
-    firebase: firebaseReducer,
-    firestore: firestoreReducer
-})
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
+});
 
 export default rootReducer;
